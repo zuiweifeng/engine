b90a99aa70049a411c41498bda1b784f2331cc7e
guminmin <moonwalkerjay@gmail.com>
2017-08-14 13:11:22 +0800

    减少outline的描边性能

--- cocos2d/core/components/CCLabelOutline.js
+++ cocos2d/core/components/CCLabelOutline.js
@@ -96,9 +96,10 @@ var LabelOutline = cc.Class({
         var label = this.node.getComponent('cc.Label');
         var sgNode = this._labelSGNode = label && label._sgNode;
         if(this._labelSGNode) {
-            sgNode.setOutlined(true);
-            sgNode.setOutlineColor(cc.color(this._color));
-            sgNode.setOutlineWidth(this._width);
+            // sgNode.setOutlined(true);
+            // sgNode.setOutlineColor(cc.color(this._color));
+            // sgNode.setOutlineWidth(this._width);
+            sgNode.enableOutline(cc.color(this._color),this._width);
             sgNode.setMargin(this._width);
         }
     },
